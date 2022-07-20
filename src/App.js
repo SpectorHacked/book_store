8da82631557db5c1ed1a022d49fca4e3dfbe63a9
erelbiran <Erel.biran@tipalti.com>
2022-07-20 13:38:28 +0300

    Orginize code

--- src/App.js
+++ src/App.js
@@ -1,87 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { TodoForm, TodoList } from './components/todo';
-import { addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo } from './lib/todoHelpers';
-import { pipe, partial } from './lib/utils';
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import CartScreen from './Screens/CartScreen';
+import LoginScreen from './Screens/LoginScreen';
+import RegisterScreen from './Screens/RegisterScreen';
+import StoreScreen from './Screens/StoreScreen';
 
-class App extends Component {
+function App(props) {
+  
+  useEffect(async() => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+    console.log(body)
+  },[])
 
-  state = {
-    currentTodo: "",
-    errorMessage: "",
-    todos: [
-      { id: 1, name: 'Learn JSX', isComplete: false },
-      { id: 2, name: 'Build an Awesome App', isComplete: false },
-      { id: 3, name: 'Ship it!', isComplete: true }
-    ]
-  }
-
-  handleToggle = (todoId) => { 
-
-    //Get updatedTodos
-    const pipeline = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
-    const updatedTodos = pipeline(todoId, this.state.todos)
-
-    this.setState({todos: updatedTodos})
-  }
-
-  handleOnchangeInput= (event)=> {
-    this.setState({currentTodo: event.target.value, errorMessage: ""})
-  }
-
-  handleSubmit = (event) => { 
-    event.preventDefault()
-
-    const newTodo = { id: generateId(), name: this.state.currentTodo, isComplete: false }
-
-    const updatedTodos = addTodo(this.state.todos, newTodo)
-    
-    this.setState({ todos: updatedTodos, currentTodo: '' })
-  }
-
-  handleEmptySubmit = (event) => {
-    event.preventDefault()
-
-    this.setState({ errorMessage: "Please supply a todo name" })
-  }
-
-  handleRemove = (id, event) => {
-    event.preventDefault()
-
-    const updatedTodos = removeTodo(this.state.todos, id)
-
-    this.setState({todos: updatedTodos})
-  }
-
-  render() {
-
-    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Todos</h1>
-        </header>
-        <div className="todoWrapper">
-          {this.state.errorMessage && <div className="errorMessage">{this.state.errorMessage}</div>}  
-          
-          <TodoForm
-            handleSubmit={ submitHandler }  
-            currentTodo={this.state.currentTodo}
-            handleOnchangeInput={this.handleOnchangeInput} />
-          
-          <TodoList
-            todos={this.state.todos}
-            handleToggle={ this.handleToggle }
-            handleOnchangeCheckbox={this.handleOnchangeCheckbox}
-            handleRemove = {this.handleRemove}
-          />
-        </div>
-      </div>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginScreen />}/>
+        <Route path="/register" element={<RegisterScreen />}/>
+        <Route path="/cart" element={<CartScreen />}/>
+        <Route path="/store" element={<StoreScreen />}/>
+      </Routes>
+  </BrowserRouter>
+  );
 }
 
 export default App;
